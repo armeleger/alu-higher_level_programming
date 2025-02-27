b9b7ce41994eb69d36df4ffd4e2a981420b9ceb7
armeleger <a.kayisire@alustudent.com>
2025-02-27 07:05:55 -0800

    Modified files

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {0:3}")
-print(f"Last 2 letters: {7:8}")
-print(f"Middle word: {0:8}")
+first_word = word[:3]
+last_word = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {first_word}")
+print(f"Last 2 letters: {last_word}")
+print(f"Middle word: {middle_word}")
+
