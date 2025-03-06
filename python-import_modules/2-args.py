0ac079fdddb52b5a4c71e7a241cc7e957d569bbf
armeleger <a.kayisire@alustudent.com>
2025-03-06 09:07:41 -0800

    Added AutoPeP8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,17 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
-    
-    # Print the number of arguments
+    num_args = len(sys.argv) - 1
+
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
     else:
         print(f"{num_args} arguments:")
-    
-    # Loop through and print each argument along with its position
-    for i in range(1, num_args + 1):
-        print(f"{i}: {sys.argv[i]}")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
 
