c09e4ee9e0974e3a4e1d722b8938cac7786cea6c
armeleger <a.kayisire@alustudent.com>
2025-03-06 07:39:31 -0800

    File 0.

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,15 @@
-if __name__ == "__main__":
+#!/usr/bin/python3
+from sys import argv
 
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    length = len(argv) - 1
+    if length == 0:
+        print("{}".format("0 arguments."))
+    elif length == 1:
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} {}".format(length, "arguments:"))
+        for i in range(1, length + 1):
+            print("{:d}: {}".format(i, argv[i]))
 
