76e49841c02121d81fe73fcfc40a86f9a61e8a36
armeleger <a.kayisire@alustudent.com>
2025-03-06 06:56:14 -0800

    Pushing Bugs Free files.

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-total_sum = 0
-
-for arg in sys.argv[1:]:
-    total_sum += int(arg)
+if __name__ == "__main__":
+    total_sum = 0
+    for arg in sys.argv[1:]:
+        total_sum += int(arg)
 
 print(total_sum)
