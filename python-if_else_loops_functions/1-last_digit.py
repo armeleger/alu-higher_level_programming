e292965708c7a43f0073e1e03067a60b293200cd
armeleger <a.kayisire@alustudent.com>
2025-03-07 06:06:14 -0800

    Pushed Bug's free files.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,3 @@ elif last_number == 0:
     print(f"Last digit of {number} is {last_number} and is 0")
 else:
     print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
-
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,3 @@ def uppercase(str):
             char = chr(ord(char) - 32)
         print(f"{char}", end="")
     print()
-
