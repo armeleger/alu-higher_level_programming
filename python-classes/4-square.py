bdf1433a787ecac68b6b46a4f05b24f443a77bef
armeleger <a.kayisire@alustudent.com>
2025-03-07 08:05:46 -0800

    Pushing Python - Classes and Objects Files

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square:
+    """A class that defines a square with a private size attribute and getters/setters for size."""
+    
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
