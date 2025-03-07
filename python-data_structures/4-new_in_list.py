3f04bd67de8d56714e994c92d28905d564e09210
armeleger <a.kayisire@alustudent.com>
2025-03-07 05:46:09 -0800

    All files pushed at once.

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        copy_lt = my_list[:]
+        return copy_lt
+    elif idx >= len(my_list):
+        copy_l = my_list[:]
+        return copy_l
+    else:
+        copy_two = my_list[:]
+        copy_two[idx] = element
+        return copy_two
+
---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,4 @@
+#!/usr/bin/pyhton3
+def multiple_returns(sentence):
+    return (len(sentence), sentence[0] if sentence else None)
+
